f928515ea5450a88fa3e179f3e6c70f01248fae6
Al Nemec <al.nemec@gmail.com>
2014-10-17 13:59:48 -0500

    Update primary

--- page-home.php
+++ page-home.php
@@ -120,9 +120,13 @@ get_header(); ?>
 					<div class="row clearfix">
 
 
+						<?php if( function_exists('get_field') && get_sub_field('2_column_image_1') ) { ?>
+								<div class="span-50 box dropin2">
+								<?php } else { ?>
+								<div class="span-50 box dropin2 noImage">
+							<?php } ?>
 
-
-						<div class="span-50 box dropin2">
+						
 
 							<?php if( function_exists('get_field') && get_sub_field('2_column_image_1') ):
 
@@ -178,7 +182,13 @@ get_header(); ?>
 
 									</div>
 
-									<div class="span-50 box dropin3">
+							<?php if( function_exists('get_field') && get_sub_field('2_column_image_2') ) { ?>
+								<div class="span-50 box dropin3">
+								<?php } else { ?>
+								<div class="span-50 box dropin3 noImage">
+							<?php } ?>
+
+									
 
 										<?php if( function_exists('get_field') && get_sub_field('2_column_image_2') ):
 
@@ -203,6 +213,204 @@ get_header(); ?>
 
 
 														<a href="<?php the_sub_field('2_column_link_2'); ?>" class="moreButton">More <?php the_sub_field('2_column_linktitle_2'); ?></a>
+
+
+
+
+
+													<div class="windows8">
+														<div class="wBall" id="wBall_1">
+														<div class="wInnerBall">
+														</div>
+														</div>
+														<div class="wBall" id="wBall_2">
+														<div class="wInnerBall">
+														</div>
+														</div>
+														<div class="wBall" id="wBall_3">
+														<div class="wInnerBall">
+														</div>
+														</div>
+														<div class="wBall" id="wBall_4">
+														<div class="wInnerBall">
+														</div>
+														</div>
+														<div class="wBall" id="wBall_5">
+														<div class="wInnerBall">
+														</div>
+														</div>
+													</div>
+
+													<div class="shade"></div>
+
+												</div>
+
+
+					</div>
+
+				<?php endif;
+
+
+				if( get_row_layout() == '3_column_container' ): ?>
+
+					<div class="row clearfix">
+
+						<?php if( function_exists('get_field') && get_sub_field('3_column_image_1') ) { ?>
+								<div class="span-33 box dropin2">
+								<?php } else { ?>
+								<div class="span-33 box dropin2 noImage">
+							<?php } ?>
+
+
+						
+
+							<?php if( function_exists('get_field') && get_sub_field('3_column_image_1') ):
+
+								$attachment_id = get_sub_field('3_column_image_1'); $size = "medium";
+								$image = wp_get_attachment_image_src($attachment_id, $size);
+
+								$backgroundImg = "background: url('" . $image[0] . "') no-repeat; background-size: auto 100%; background-position: center center";
+								$slide = '<img height="' . $image[1] . '" width="' . $image[2] . '" src="' . $image[0] . '" alt=" ">';
+								echo $slide;
+							endif; ?>
+
+							<h2><?php the_sub_field('3_column_linktitle_1'); ?></h2>
+
+							<div class="boxContent">
+
+															<?php the_sub_field('3_column_caption_1'); ?>
+																										</div>
+																		<div class="topShade"></div>
+											<div class="bottomShade"></div>
+
+
+<a href="<?php the_sub_field('3_column_link_1'); ?>" class="moreButton">More <?php the_sub_field('3_column_linktitle_1'); ?></a>
+
+
+
+
+
+
+										<div class="windows8">
+											<div class="wBall" id="wBall_1">
+											<div class="wInnerBall">
+											</div>
+											</div>
+											<div class="wBall" id="wBall_2">
+											<div class="wInnerBall">
+											</div>
+											</div>
+											<div class="wBall" id="wBall_3">
+											<div class="wInnerBall">
+											</div>
+											</div>
+											<div class="wBall" id="wBall_4">
+											<div class="wInnerBall">
+											</div>
+											</div>
+											<div class="wBall" id="wBall_5">
+											<div class="wInnerBall">
+											</div>
+											</div>
+										</div>
+
+										<div class="shade"></div>
+
+									</div>
+
+									<?php if( function_exists('get_field') && get_sub_field('3_column_image_2') ) { ?>
+								<div class="span-33 box dropin3">
+								<?php } else { ?>
+								<div class="span-33 box dropin3 noImage">
+							<?php } ?>
+									
+
+										<?php if( function_exists('get_field') && get_sub_field('3_column_image_2') ):
+
+											$attachment_id = get_sub_field('3_column_image_2'); $size = "medium";
+											$image = wp_get_attachment_image_src($attachment_id, $size);
+
+											$backgroundImg = "background: url('" . $image[0] . "') no-repeat; background-size: auto 100%; background-position: center center";
+											$slide = '<img height="' . $image[1] . '" width="' . $image[2] . '" src="' . $image[0] . '" alt=" ">';
+											echo $slide;
+										endif; ?>
+
+										<h2><?php the_sub_field('3_column_linktitle_2'); ?></h2>
+
+
+										<div class="boxContent">
+
+																	<?php the_sub_field('3_column_caption_2'); ?>
+																													</div>
+																					<div class="topShade"></div>
+														<div class="bottomShade"></div>
+
+
+
+														<a href="<?php the_sub_field('3_column_link_2'); ?>" class="moreButton">More <?php the_sub_field('3_column_linktitle_2'); ?></a>
+
+
+
+
+
+													<div class="windows8">
+														<div class="wBall" id="wBall_1">
+														<div class="wInnerBall">
+														</div>
+														</div>
+														<div class="wBall" id="wBall_2">
+														<div class="wInnerBall">
+														</div>
+														</div>
+														<div class="wBall" id="wBall_3">
+														<div class="wInnerBall">
+														</div>
+														</div>
+														<div class="wBall" id="wBall_4">
+														<div class="wInnerBall">
+														</div>
+														</div>
+														<div class="wBall" id="wBall_5">
+														<div class="wInnerBall">
+														</div>
+														</div>
+													</div>
+
+													<div class="shade"></div>
+
+												</div>
+
+												<?php if( function_exists('get_field') && get_sub_field('3_column_image_3') ) { ?>
+								<div class="span-33 box dropin4">
+								<?php } else { ?>
+								<div class="span-33 box dropin4 noImage">
+							<?php } ?>
+
+												
+
+										<?php if( function_exists('get_field') && get_sub_field('3_column_image_2') ):
+
+											$attachment_id = get_sub_field('3_column_image_2'); $size = "medium";
+											$image = wp_get_attachment_image_src($attachment_id, $size);
+
+											$backgroundImg = "background: url('" . $image[0] . "') no-repeat; background-size: auto 100%; background-position: center center";
+											$slide = '<img height="' . $image[1] . '" width="' . $image[2] . '" src="' . $image[0] . '" alt=" ">';
+											echo $slide;
+										endif; ?>
+
+										<h2><?php the_sub_field('3_column_linktitle_2'); ?></h2>
+
+
+										<div class="boxContent">
+
+																	<?php the_sub_field('3_column_caption_2'); ?>
+																													</div>
+																					<div class="topShade"></div>
+														<div class="bottomShade"></div>
+
+
+
+														<a href="<?php the_sub_field('3_column_link_2'); ?>" class="moreButton">More <?php the_sub_field('3_column_linktitle_2'); ?></a>
 
 
 
